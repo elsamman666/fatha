0a758e5e6cc686c24fb6a7ae8b600c0377817bce
elsamman666 <69174694+elsamman666@users.noreply.github.com>
2021-02-09 18:59:13 +0200

    Delete lib directory

--- lib/LeftToRight.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:localize_and_translate/localize_and_translate.dart';
-
-TextDirection direction = translator.currentLanguage=='en'? TextDirection.ltr:TextDirection.rtl;
-
-tr(String word){
-  return translator.translate(word);
-}
